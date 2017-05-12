259a8cc0f18f8993c9c6ab69da68281ee15c9c40
steve <551619855@qq.com>
2017-05-12 09:33:32 +0800

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,165 @@
+#include <iostream>
+#include     <stdio.h>      /*标准输入输出定义*/
+#include     <stdlib.h>     /*标准函数库定义*/
+#include     <unistd.h>     /*Unix 标准函数定义*/
+#include     <sys/types.h>
+#include     <sys/stat.h>
+
+#include <fcntl.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <sys/stat.h>
+#include <fcntl.h>1
+
+#include <unistd.h>
+#include <termios.h>
+#include <stdlib.h>
+#include "time.h"
+#include "stdio.h"
+#include "JY901.h"
+unsigned char ucComNo[2] ={0,0};
+int set_opt(int fd,int nSpeed, int nBits, char nEvent, int nStop)
+{
+    struct termios newtio,oldtio;
+    if  ( tcgetattr( fd,&oldtio)  !=  0)
+    {
+        perror("SetupSerial 1");
+        return -1;
+    }
+    bzero( &newtio, sizeof( newtio ) );
+    newtio.c_cflag  |=  CLOCAL | CREAD;
+    newtio.c_cflag &= ~CSIZE;
+
+    switch( nBits )
+    {
+        case 7:
+            newtio.c_cflag |= CS7;
+            break;
+        case 8:
+            newtio.c_cflag |= CS8;
+            break;
+    }
+
+    switch( nEvent )
+    {
+        case 'O':                     //奇校验
+            newtio.c_cflag |= PARENB;
+            newtio.c_cflag |= PARODD;
+            newtio.c_iflag |= (INPCK | ISTRIP);
+            break;
+        case 'E':                     //偶校验
+            newtio.c_iflag |= (INPCK | ISTRIP);
+            newtio.c_cflag |= PARENB;
+            newtio.c_cflag &= ~PARODD;
+            break;
+        case 'N':                    //无校验
+            newtio.c_cflag &= ~PARENB;
+            break;
+    }
+
+    switch( nSpeed )
+    {
+        case 2400:
+            cfsetispeed(&newtio, B2400);
+            cfsetospeed(&newtio, B2400);
+            break;
+        case 4800:
+            cfsetispeed(&newtio, B4800);
+            cfsetospeed(&newtio, B4800);
+            break;
+        case 9600:
+            cfsetispeed(&newtio, B9600);
+            cfsetospeed(&newtio, B9600);
+            break;
+        case 115200:
+            cfsetispeed(&newtio, B115200);
+            cfsetospeed(&newtio, B115200);
+            break;
+        default:
+            cfsetispeed(&newtio, B9600);
+            cfsetospeed(&newtio, B9600);
+            break;
+    }
+    if( nStop == 1 )
+    {
+        newtio.c_cflag &=  ~CSTOPB;
+    }
+    else if ( nStop == 2 )
+    {
+        newtio.c_cflag |=  CSTOPB;
+    }
+    newtio.c_cc[VTIME]  = 0;
+    newtio.c_cc[VMIN] = 0;
+    tcflush(fd,TCIFLUSH);
+    if((tcsetattr(fd,TCSANOW,&newtio))!=0)
+    {
+        perror("com set error");
+        return -1;
+    }
+    printf("set done!\n");
+    return 0;
+}
+int main(int argc, char* argv[])
+{
+    CJY901 JY901;
+
+    int fd = open("/dev/ttyUSB0",O_RDWR|O_NOCTTY);
+    if(-1==fd)
+    {
+        std::cout << " can't open device"<< std::endl;
+    }
+
+    char chrBuffer[2000];
+    unsigned short usLength=0,usCnt=0;
+    unsigned long ulBaund=115200,ulComNo=3;
+    signed char cResult= 1;
+
+    set_opt(fd,115200,8,'N',1);
+//    while(cResult!=0)
+//    {
+//        cResult = OpenCOMDevice(ulComNo,ulBaund);
+//    }
+
+    float last_milisecond = (float)JY901.stcTime.usMiliSecond/1000;
+    while(1)
+    {
+
+//        usLength = CollectUARTData(ulComNo,chrBuffer);
+        usLength = read(fd,chrBuffer,1000);
+        if (usLength>0)
+        {
+            JY901.CopeSerialData(chrBuffer,usLength);
+        }
+//        Sleep(100);
+        usleep(1);
+
+        if (usCnt++>=0&& last_milisecond!=(float)JY901.stcTime.usMiliSecond/1000)
+        {
+            usCnt=0;
+            printf("Time:20%d-%d-%d %d:%d:%.3f\r\n",(short)JY901.stcTime.ucYear,(short)JY901.stcTime.ucMonth,
+                   (short)JY901.stcTime.ucDay,(short)JY901.stcTime.ucHour,(short)JY901.stcTime.ucMinute,(float)JY901.stcTime.ucSecond+(float)JY901.stcTime.usMiliSecond/1000);
+
+//            printf("Acc:%.3f %.3f %.3f\r\n",(float)JY901.stcAcc.a[0]/32768*16,(float)JY901.stcAcc.a[1]/32768*16,(float)JY901.stcAcc.a[2]/32768*16);
+
+//            printf("Gyro:%.3f %.3f %.3f\r\n",(float)JY901.stcGyro.w[0]/32768*2000,(float)JY901.stcGyro.w[1]/32768*2000,(float)JY901.stcGyro.w[2]/32768*2000);
+
+//            printf("Angle:%.3f %.3f %.3f\r\n",(float)JY901.stcAngle.Angle[0]/32768*180,(float)JY901.stcAngle.Angle[1]/32768*180,(float)JY901.stcAngle.Angle[2]/32768*180);
+
+//            printf("Mag:%d %d %d\r\n",JY901.stcMag.h[0],JY901.stcMag.h[1],JY901.stcMag.h[2]);
+
+//            printf("Pressure:%lx Height%.2f\r\n",JY901.stcPress.lPressure,(float)JY901.stcPress.lAltitude/100);
+
+//            printf("DStatus:%d %d %d %d\r\n",JY901.stcDStatus.sDStatus[0],JY901.stcDStatus.sDStatus[1],JY901.stcDStatus.sDStatus[2],JY901.stcDStatus.sDStatus[3]);
+
+//            printf("Longitude:%ldDeg%.5fm Lattitude:%ldDeg%.5fm\r\n",JY901.stcLonLat.lLon/10000000,(double)(JY901.stcLonLat.lLon % 10000000)/1e5,JY901.stcLonLat.lLat/10000000,(double)(JY901.stcLonLat.lLat % 10000000)/1e5);
+
+//            printf("GPSHeight:%.1fm GPSYaw:%.1fDeg GPSV:%.3fkm/h\r\n\r\n",(float)JY901.stcGPSV.sGPSHeight/10,(float)JY901.stcGPSV.sGPSYaw/10,(float)JY901.stcGPSV.lGPSVelocity/1000);
+
+            last_milisecond=(float)JY901.stcTime.usMiliSecond/1000;
+        }
+
+    }
+    return 0;
+}
