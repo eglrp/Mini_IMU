318ae692c0a46a11b576f4d6b95c359c1aa15497
steve <551619855@qq.com>
2017-05-17 22:46:37 +0800

    Can read data ...

--- IMUPlus.cpp
+++ IMUPlus.cpp
@@ -143,13 +143,20 @@ void ReadThread(int fd,CharQueue<char> & cq){
 //   std::thread::
     usleep(100000);
     std::cout << "start read" << std::endl;
+    char  buff[4096];
     while(true)
     {
-        char  buff[4096];
+
         int length = read(fd,buff,4000);
         if(length>0)
         {
             cq.AddBuf(buff,length);
+//            for(int i(0);i<length;++i)
+//            {
+//                printf("%02x ",buff[i]);
+//            }
+
+//            std::cout << " buff lenght :" << length << std::endl;
         }
         usleep(100);
     }
@@ -197,21 +204,21 @@ int main(int argc, char* argv[])
 
     sleep(1);
 
-    while(1)
-    {
-        char  t[2];
-        cq.ReadBuf(t,2);
-//        std::cout << "size " << cq.getSize() << std::endl;
-        if((t[0])==0x55){
-            std::cout << "break" << std::endl;
-            break;
-        }else{
-//            std::cout << t[0] << std::endl;
-//            printf("is : %02X\n",t[0]);
-            cq.DeletBuf(1);
-        }
-        usleep(1000);
-    }
+//    while(1)
+//    {
+//        char  t[2];
+//        cq.ReadBuf(t,2);
+////        std::cout << "size " << cq.getSize() << std::endl;
+//        if((t[0])==0x55){
+//            std::cout << "break" << std::endl;
+//            break;
+//        }else{
+////            std::cout << t[0] << std::endl;
+////            printf("is : %02X\n",t[0]);
+//            cq.DeletBuf(1);
+//        }
+//        usleep(1000);
+//    }
 
 
 
@@ -230,9 +237,13 @@ int main(int argc, char* argv[])
         if(cq.ReadBuf(chrBuffer,5))
         {
             cq.DeletBuf(5);
+            usLength = 5;
         }else{
 //            std::cout << "wait ?" << std::endl;
-            usleep(1000);
+            if(cq.getSize()==0)
+            {
+                usleep(1000);
+            }
             continue;
         }
 
@@ -262,25 +273,25 @@ int main(int argc, char* argv[])
                 }
 
             }
-            printf("Time:20%d-%d-%d %d:%d:%.3f",(short)JY901.stcTime.ucYear,(short)JY901.stcTime.ucMonth,
-                   (short)JY901.stcTime.ucDay,(short)JY901.stcTime.ucHour,(short)JY901.stcTime.ucMinute,(float)JY901.stcTime.ucSecond+(float)JY901.stcTime.usMiliSecond/1000);
-
-
-            printf("Acc:%.3f %.3f %.3f",(float)JY901.stcAcc.a[0]/32768*16,(float)JY901.stcAcc.a[1]/32768*16,(float)JY901.stcAcc.a[2]/32768*16);
-
-            printf("Gyro:%.3f %.3f %.3f",(float)JY901.stcGyro.w[0]/32768*2000,(float)JY901.stcGyro.w[1]/32768*2000,(float)JY901.stcGyro.w[2]/32768*2000);
-
-            printf("Angle:%.3f %.3f %.3f\r\n",(float)JY901.stcAngle.Angle[0]/32768*180,(float)JY901.stcAngle.Angle[1]/32768*180,(float)JY901.stcAngle.Angle[2]/32768*180);
-
-            printf("Mag:%d %d %d",JY901.stcMag.h[0],JY901.stcMag.h[1],JY901.stcMag.h[2]);
-
-            printf("Pressure:%lx Height%.2f",JY901.stcPress.lPressure,(float)JY901.stcPress.lAltitude/100);
-
-            printf("DStatus:%d %d %d %d\r\n",JY901.stcDStatus.sDStatus[0],JY901.stcDStatus.sDStatus[1],JY901.stcDStatus.sDStatus[2],JY901.stcDStatus.sDStatus[3]);
-
-            printf("Longitude:%ldDeg%.5fm Lattitude:%ldDeg%.5fm\r\n",JY901.stcLonLat.lLon/10000000,(double)(JY901.stcLonLat.lLon % 10000000)/1e5,JY901.stcLonLat.lLat/10000000,(double)(JY901.stcLonLat.lLat % 10000000)/1e5);
-
-            printf("GPSHeight:%.1fm GPSYaw:%.1fDeg GPSV:%.3fkm/h\r\n\r\n",(float)JY901.stcGPSV.sGPSHeight/10,(float)JY901.stcGPSV.sGPSYaw/10,(float)JY901.stcGPSV.lGPSVelocity/1000);
+//            printf("Time:20%d-%d-%d %d:%d:%.3f",(short)JY901.stcTime.ucYear,(short)JY901.stcTime.ucMonth,
+//                   (short)JY901.stcTime.ucDay,(short)JY901.stcTime.ucHour,(short)JY901.stcTime.ucMinute,(float)JY901.stcTime.ucSecond+(float)JY901.stcTime.usMiliSecond/1000);
+//
+//
+//            printf("Acc:%.3f %.3f %.3f",(float)JY901.stcAcc.a[0]/32768*16,(float)JY901.stcAcc.a[1]/32768*16,(float)JY901.stcAcc.a[2]/32768*16);
+//
+//            printf("Gyro:%.3f %.3f %.3f",(float)JY901.stcGyro.w[0]/32768*2000,(float)JY901.stcGyro.w[1]/32768*2000,(float)JY901.stcGyro.w[2]/32768*2000);
+//
+//            printf("Angle:%.3f %.3f %.3f\r\n",(float)JY901.stcAngle.Angle[0]/32768*180,(float)JY901.stcAngle.Angle[1]/32768*180,(float)JY901.stcAngle.Angle[2]/32768*180);
+//
+//            printf("Mag:%d %d %d",JY901.stcMag.h[0],JY901.stcMag.h[1],JY901.stcMag.h[2]);
+//
+//            printf("Pressure:%lx Height%.2f",JY901.stcPress.lPressure,(float)JY901.stcPress.lAltitude/100);
+//
+//            printf("DStatus:%d %d %d %d\r\n",JY901.stcDStatus.sDStatus[0],JY901.stcDStatus.sDStatus[1],JY901.stcDStatus.sDStatus[2],JY901.stcDStatus.sDStatus[3]);
+//
+//            printf("Longitude:%ldDeg%.5fm Lattitude:%ldDeg%.5fm\r\n",JY901.stcLonLat.lLon/10000000,(double)(JY901.stcLonLat.lLon % 10000000)/1e5,JY901.stcLonLat.lLat/10000000,(double)(JY901.stcLonLat.lLat % 10000000)/1e5);
+//
+//            printf("GPSHeight:%.1fm GPSYaw:%.1fDeg GPSV:%.3fkm/h\r\n\r\n",(float)JY901.stcGPSV.sGPSHeight/10,(float)JY901.stcGPSV.sGPSYaw/10,(float)JY901.stcGPSV.lGPSVelocity/1000);
 
             out_file << (short) JY901.stcTime.ucYear << "-" << (short) JY901.stcTime.ucMonth << "-" <<
                      (short) JY901.stcTime.ucDay << "-" <<
