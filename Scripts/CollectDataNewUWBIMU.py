bacfa5a5554734cdb30a5701d33dbd5414d4722d
steve <551619855@qq.com>
2017-12-16 15:53:25 +0800

    collecting two IMU.

---
+++ Scripts/CollectDataNewUWBIMU.py
@@ -0,0 +1,43 @@
+# -*- coding:utf-8 -*-
+# Create by steve in 17-5-12 at 下午8:43
+
+
+import os
+
+
+
+if __name__ == '__main__':
+    base_dir = './IMUWB/'
+    a = 10
+    for dir in os.listdir(base_dir):
+        if float(dir)>=a:
+            a = int(dir)+1
+
+    sub_dir_name = base_dir+str(a)
+
+    os.mkdir(sub_dir_name)
+
+    IMU_dev = '/dev/ttyUSB0'
+    IMU_file = sub_dir_name+'/imu.txt'
+
+    UWB_dev = '/dev/ttyUSB1'
+    UWB_dir = sub_dir_name+'/'
+
+    os.system('../cmake-build-debug/IMUPlus {0} {1} & ../cmake-build-debug/UWBPlus {2} {3}'.format(
+        IMU_dev,
+        IMU_file,
+        UWB_dev,
+        UWB_dir
+    ))
+
+
+
+
+
+
+
+
+
+
+
+
